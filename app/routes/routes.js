43ef528506d8715a7f677c3c3eaeab1bb0b18c67
Kainryuk <147082321+Kainryuk@users.noreply.github.com>
2025-09-13 18:21:31 -0300

    Update routes.js

--- app/routes/routes.js
+++ app/routes/routes.js
@@ -1,10 +1,7 @@
-const { home } = require('../controllers/home.js');
-<<<<<<< HEAD 
-=======
-const { comentarioPost } = require('../controllers/mensagem.js');
->>>>>>> 139b3c0 (pit)
-const { tarsila } = require('../controllers/tarsila.js');
-const { portinari } = require('../controllers/portinari.js');
+const { home } = require('../controllers/homeController.js');
+const { tarsila } = require('../controllers/tarsilaController.js');
+const { portinari } = require('../controllers/portinariController.js');
+const mensagemController = require("../controllers/mensagemController.js");
 
 module.exports = {
   home: (app) => {
@@ -20,26 +17,22 @@ module.exports = {
       tarsila(app, req, res);
     });
   },
-  routeNotFound: (app) => {
-    app.get('.', function(req, res) {
+  routeNotFound: (app) => {""
+    app.get('.', function(res) {
       console.log('Rota não encontrada');
-      res.render('notfound.ejs');
-    })
-  },  portinari: (app) => {
+      res.status(404).render('./notfound.ejs');
+    });
+  },  
+  portinari: (app) => {
     app.get('/portinari', function (req, res) {
       console.log('Rota /portinari acionada');
       portinari(app, req, res);
     });
-<<<<<<< HEAD
-=======
   },
-  mensagem: (app) => {
-    
-    app.post('/', function (req, res) {
-      console.log('Rota / acionada via POST');
-      comentarioPost(app, req, res); //Controller da mensagem
+  enviaMensagem: (app) => {
+    app.post("/enviaMensagem", (req, res) => {
+      console.log("Rota /enviaMensagem acionada");
+      mensagemController(app, req, res); // agora funciona
     });
->>>>>>> 139b3c0 (pit)
   }
-
-}
+};
