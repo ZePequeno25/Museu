a25e3f2403c150e0efc71482eedc7dbb7e005ac5
Kainryuk <147082321+Kainryuk@users.noreply.github.com>
2025-09-13 18:21:10 -0300

    Update mensagem.js

--- app/models/mensagem.js
+++ app/models/mensagem.js
@@ -1,7 +1,6 @@
-module.exports.insertComment = (dbConn, comentary, id_obradearte, callback) => {
-  console.log('[Model Mensagem - Insert Comment]');
-  const query = 'INSERT INTO comments (comentary, id_obradearte) VALUES (?, ?)';
-  dbConn.query(query, [comentary, id_obradearte], (error, result) => {
-    callback(error, result); // Chama callback com erro ou resultado
-  });
-};
+module.exports = {
+    salvar: (conn, comentary, id_obradearte, callback) => {
+      const sql = "INSERT INTO comments (comentary, id_obradearte) VALUES (?, ?)";
+      conn.query(sql, [comentary, id_obradearte], callback);
+    }
+  };
